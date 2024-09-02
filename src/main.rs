bd01a76b221ccb1f03d32590d133df01fc0adb72
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-09-02 22:03:40 +0200

    Test filter

--- src/main.rs
+++ src/main.rs
@@ -26,3 +26,39 @@ pub fn run_grep<R: BufRead, W: Write>(query: &str, reader: R, mut writer: W) -> 
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_run_grep() {
+        let test_cases = [
+            (
+                "Rust",
+                "In the world of code so vast and wide,\nRust stands strong, with memory on its side.",
+                "Rust stands strong, with memory on its side.\n",
+            ),
+            (
+                "Rust",
+                "No nulls to fear, no data race,\nSafety and speed in every trace.",
+                "",
+            ),
+            (
+                "Rust",
+                "In Rust we trust, a language to love.\nCompile time checks, borrow and own,\nIn Rust's embrace, we find our home.",
+                "In Rust we trust, a language to love.\nIn Rust's embrace, we find our home.\n"
+            )
+        ];
+
+        for (query, input, expected) in &test_cases {
+            let mut output = Vec::new();
+
+            run_grep(query, Cursor::new(input), &mut output).expect("run_grep failed");
+
+            let output_str = String::from_utf8(output).expect("Failed to convert output to string");
+            assert_eq!(output_str, *expected)
+        }
+    }
+}

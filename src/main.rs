863c34e9ccf03043d41b2b832443c877d9a586b6
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-09-02 20:12:43 +0200

    Read cmd line arg

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::env;
 use std::io::{self, BufRead, Write};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    println!("Will search for {}", query);
+
     let stdin = io::stdin();
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
